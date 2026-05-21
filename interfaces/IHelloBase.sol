eb625eb5175cca757fd28eaad6f07ac00515eadb
SalfianF <salfianf@users.noreply.github.com>
2026-05-21 11:03:39 +0000

    refactor: add IHelloBase interface

---
+++ interfaces/IHelloBase.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title IHelloBase
+ * @notice Interface for HelloBase contract
+ * @dev Defines the external functions for a simple Base deployment proof contract
+ */
+interface IHelloBase {
+    /**
+     * @notice Emitted when the stored message is updated
+     * @param updater Address that updated the message
+     * @param newMessage The new message string
+     */
+    event MessageUpdated(address indexed updater, string newMessage);
+
+    /**
+     * @notice Get the stored message
+     * @return string The current message
+     */
+    function message() external view returns (string memory);
+
+    /**
+     * @notice Get the contract owner
+     * @return address The owner address
+     */
+    function owner() external view returns (address);
+
+    /**
+     * @notice Get the deployment timestamp
+     * @return uint256 The block timestamp of deployment
+     */
+    function deployTime() external view returns (uint256);
+
+    /**
+     * @notice Update the stored message
+     * @dev Only callable by the contract owner
+     * @param _message New message to store
+     */
+    function setMessage(string calldata _message) external;
+
+    /**
+     * @notice Get all contract info at once
+     * @return string Current message
+     * @return address Owner address
+     * @return uint256 Deployment timestamp
+     */
+    function getInfo() external view returns (string memory, address, uint256);
+}
